754b87a17be8cf06d19c00d6e579d9c84b82ce2f
phungdactai <phungdactai98@gmail.com>
2020-12-25 18:14:32 +0700

    lam den js

---
+++ MISA-Fresher-nhanh1/MISA.CukCuk.Web/js/page/customer.js
@@ -0,0 +1,38 @@
+﻿$(document).ready(function () {
+    loadData();
+})
+
+/**
+ * 
+ * */
+function loadData() {
+    //Lấy dữ liệu
+    $.ajax({
+        url: "http://api.manhnv.net/api/employees",
+        method: "GET",
+    }).done(function (res){
+        //Chạy đúng
+        var data = res;
+        $.each(data, function (index, item){
+            var tr = $(`<tr>
+                            <td>`+ item.EmployeeCode +`</td>
+                            <td>`+ item['FullName']+`</td>
+                            <td>`+ item['Gender'] +`</td>
+                            <td>`+ item['DateOfBirth'] +`</td>
+                            <td></td>
+                            <td>`+ item['PhoneNumber'] +`</td>
+                            <td>`+ item['Email'] +`</td>
+                            <td style="max-width:215px;"><span style="width:215px">`+ item['Address'] +`</span></td>
+                            <td>`+ item['Salary'] +`</td>
+                            <td>206225549</td>
+                        </tr>`);
+            $('table tbody').append(tr);
+        })
+        
+ 
+    }).fail(function (res){
+        //Chạy fail
+    })
+    // Binding dl lên table
+    
+}
